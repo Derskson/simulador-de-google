e5f3cbe294fb3101e90f923d407cf58b47f1a6ce
Derskson <anderssong.jaime@gmail.com>
2025-01-03 15:52:25 -0500

    actualizando el codigo js

--- js/voice-search.js
+++ js/voice-search.js
@@ -2,7 +2,7 @@
 const voiceSearch = document.querySelector(".voice-search");
 let microAceptado = false;
 
-const voiceSearchModalOpen = ()=>{
+const voiceSearchModalOpen = () =>{
 	voiceSearch.style.display = "flex";
 	voiceSearch.style.animation = "aparecer 0.5s forwards";
 	voiceRecognition();
@@ -12,7 +12,7 @@ const voiceSearchModalOpen = ()=>{
 const voiceSearchModalClose = () =>{
 	voiceSearch.style.animation = "desaparecer 0.25s forwards";
 	setTimeout(()=>{
-	    voiceSearch.style.display = "none";
+	voiceSearch.style.display = "none";
 	},250)
 }
 
